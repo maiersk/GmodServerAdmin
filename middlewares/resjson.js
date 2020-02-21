8918ac679e6cf4b863114c3826296158be6f2a00
maiersk <poilyoungman@foxmail.com>
2020-02-21 20:21:57 +0800

    res logger

--- middlewares/resjson.js
+++ middlewares/resjson.js
@@ -1,9 +1,13 @@
+const logger = new (require('../lib/Logger'))('Result', false, 'resjson.js');
+
 module.exports = {
     msg(msg) {
+        logger.log('exec succeed ' + msg);
         return {succeed : true, massage : msg};
     },
     
     data(data) {
+        logger.debug('exec succeed ', data);
         return {succeed : true, data : data};
     },
 
